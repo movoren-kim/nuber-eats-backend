af54d626d0372fbb220dc269ce6d9dbb2f9152b0
Morvoren Kim <morvoren.kim@gmail.com>
2024-07-14 01:49:41 +0900

    FIX: 아래 참고
      beforeEach(() => {
        jest.clearAllMocks();
      });

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -14,12 +14,15 @@ const mockRepository = {
 const mockAuthService = {
   generateTokens: jest.fn(),
 };
+
 type MockRepository<T = any> = Partial<
   Record<keyof Repository<User>, jest.Mock>
 >;
+
 describe('UsersService', () => {
   let service: UsersService;
   let usersRepository: MockRepository<User>;
+
   beforeAll(async () => {
     const testingModules = await Test.createTestingModule({
       providers: [
@@ -37,20 +40,26 @@ describe('UsersService', () => {
     service = testingModules.get<UsersService>(UsersService);
     usersRepository = testingModules.get(getRepositoryToken(User));
   });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
   describe('createAccount', () => {
     const createAccountArgs = {
-      email: '',
-      password: '',
+      email: 'bs@email.com',
+      password: 'bs.password',
       role: UserRole.Client,
     };
+
     it('should fail if user exists', async () => {
       usersRepository.findOne.mockResolvedValue({
         id: 1,
-        email: 'alsldalsda',
+        email: 'bs@email.com',
       });
       const result = await service.createAccount(createAccountArgs);
       expect(result).toMatchObject({
@@ -58,14 +67,32 @@ describe('UsersService', () => {
         error: '[계정 생성] 이미 존재하는 이메일입니다.',
       });
     });
-    it('should create a new user', async () => {
+
+    it('should fail if database does not execute', async () => {
       usersRepository.findOne.mockResolvedValue(undefined);
       usersRepository.create.mockReturnValue(createAccountArgs);
-      await service.createAccount(createAccountArgs);
+      usersRepository.save.mockResolvedValue(null);
+      const result = await service.createAccount(createAccountArgs);
       expect(usersRepository.create).toHaveBeenCalledTimes(1);
       expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
       expect(usersRepository.save).toHaveBeenCalledTimes(1);
       expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+      expect(result).toMatchObject({
+        ok: false,
+        error: '[계정 생성] 계정 데이터 생성 중 오류가 발생하였습니다.',
+      });
+    });
+
+    it('should create a new user', async () => {
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+      const result = await service.createAccount(createAccountArgs);
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+      expect(result).toEqual({ ok: true });
     });
   });
 
